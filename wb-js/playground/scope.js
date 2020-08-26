674406c182c0ed858e7e898f99f0adf1b53b1009
Ryan Apellanes <ryanapellanes.dev@gmail.com>
2020-08-26 16:35:50 -0700

    scope html and js; hoisting init

---
+++ wb-js/playground/scope.js
@@ -0,0 +1,49 @@
+// const age = 100;
+
+// Function Scope
+// function go() {
+//   const myAge = 200;
+//   const hair = "blonde";
+//   console.log(age);
+//   console.log(myAge);
+//   console.log(hair);
+// }
+
+// go();
+
+/* eslint-disable */
+
+// function isCool(name) {
+//   if (name === 'ry') {
+//     var cool = true;
+//   }
+//   console.log(cool);
+//   return cool;
+// }
+
+// for(let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+const dog = "snickers";
+
+function logDog(dog) {
+  console.log(dog);
+}
+
+function go() {
+  const dog = "sunny";
+  logDog("sunny");
+}
+
+go();
+
+function yell() {
+  console.log(name.toUpperCase());
+}
+
+function sayHi(name) {
+  yell();
+}
+
+yell();
